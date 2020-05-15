2f272cf13909c579e138ffd5edba3ca203b7a901
dominicadiaz <dad358@cornell.edu>
2020-05-15 14:48:30 -0400

    arrays correcty allocated

--- solver.c
+++ solver.c
@@ -43,6 +43,11 @@ int main() {
     double** u_star = (double**)calloc(ny, sizeof(double*));
     double** v_star = (double**)calloc(ny, sizeof(double*));
 
+    double** du_s = (double**)calloc(ny, sizeof(double*));
+    double** dv_s = (double**)calloc(ny, sizeof(double*));
+    double** du_ss = (double**)calloc(ny, sizeof(double*));
+    double** dv_ss = (double**)calloc(ny, sizeof(double*));
+
     double** H_old = (double**)calloc(ny, sizeof(double*));
 
     double** p = (double**)calloc(ny, sizeof(double*));             //cell centered
@@ -50,11 +55,16 @@ int main() {
 
 
 
-    for (i = 0; i < nx; i++) {
+    for (i = 0; i < ny; i++) {
         u[i] = (double*)calloc(nx, sizeof(double));
         v[i] = (double*)calloc(nx, sizeof(double));
         u_star[i] = (double*)calloc(nx, sizeof(double));
         v_star[i] = (double*)calloc(nx, sizeof(double));
+        
+        du_s[i] = (double*)calloc(nx, sizeof(double));
+        dv_s[i] = (double*)calloc(nx, sizeof(double));
+        du_ss[i] = (double*)calloc(nx, sizeof(double));
+        dv_ss[i] = (double*)calloc(nx, sizeof(double));
 
         H_old[i] = (double*)calloc(nx, sizeof(double));
 
@@ -72,19 +82,25 @@ int main() {
     for(i = 0; i < Nx; i++) {
         for (j = 0; j < Ny; j++) {
             u[j][i] = 0;
-            printf("Hello world");
+            printf("(%d, %d)", j, i);
         }
+        printf("\n");
     }
     
 
 
 
     /* ----------------------- Freeing Variables -------------------------------- */
-    for (i = 0; i < Nx; i++) {
+    for (i = 0; i < Ny; i++) {
         free(u[i]);
         free(v[i]);
         free(u_star[i]);
         free(v_star[i]);
+
+        free(du_s[i]);
+        free(dv_s[i]);
+        free(du_ss[i]);
+        free(dv_ss[i]);
 
         free(H_old[i]);
 
@@ -96,6 +112,11 @@ int main() {
     free(v);
     free(u_star);
     free(v_star);
+
+    free(du_s);
+    free(dv_s);
+    free(du_ss);
+    free(dv_s);
 
     free(H_old);
 
