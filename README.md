f554ab9d56e209fc23cfa9586bc095a9a08e6a11
dominicadiaz <dad358@cornell.edu>
2020-05-15 18:29:14 -0400

    more inititializations

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # 2D_NavierStokesSolver_ScalarTransport
 
 Objective of the project: create a second-order unstead Navier-Stokes solver on a semi-complex geometry including scalartransport.
+
+
+Some notes:
+1. There are four new arrays that I created for \Delta u**, \Delta v**, \Delta u*, and \Delta v* and I called them du_ss, dv_ss, du_s, and dv_s for simplicity. IF you think of a better notation, feel free to change it!
+2. I've created a variable called h_old which will store H^{n-1} since we will need that for the nth step.
+
+
+Two things that I'm not sure about that we'll have to think about:
+1. the document mentions "you should adjust the outflow velicty so that its bulk value is equal to U (implying that mass is exactly conserved)" but I'm not sure how to do this. 
+2. The document doesn't eplain when the scalar transport step should be implemented in the code but it seems like it should be right after step 3 and should be solved at each time step.
--- solver.c
+++ solver.c
@@ -14,7 +14,9 @@
 
 
 int main() {
-    /* --------------------- Initializing Variables  ------------------------------ */
+    /* -------------------------------------------------------------------------
+    -------------------------- Initializing Variables --------------------------
+    ------------------------------------------------------------------------- */
     int i = 0, j = 0, k = 0; /* Counters for all "for loops" */
     int iter = 0;
 
@@ -33,10 +35,28 @@ int main() {
     double dx = H / 20;
     double dy = H / 20;
 
+
+
+    /* Initializing variables for this step 1 */
+    double Hx;
+    double Hy;
+    double u_cc; /* cc = Cell cented velocity */
+    double v_cc;
+    double u_cc_im1; /* im1 = "i minus 1"; cell centered velocity at 1 position back in the i direction */
+    double v_cc_jm1; /* jm1 = "j minus 1"; cell centered velocity at 1 position back in the j direction */
+    double u_s;  /* s = staggered velocity (i-1/2, j-1/2) from the cc values */
+    double v_s;
+    double u_s_ip1; /* ip1 = "i plus 1"; staggered velocity at 1 position forward in the i direction */
+    double u_s_jp1; /* jp1 = "j plus 1"; staggered velocity at 1 position forward in the j direction */
+    double v_s_ip1;
+    double v_s_jp1;
+
     
 
 
-    /* ---------------------- Initializing Arrays -------------------------------- */
+    /* -------------------------------------------------------------------------
+    -------------------------- Initializing Arrays -----------------------------
+    ------------------------------------------------------------------------- */
 
     double** u = (double**)calloc(ny, sizeof(double*));
     double** v = (double**)calloc(ny, sizeof(double*));
@@ -48,10 +68,14 @@ int main() {
     double** du_ss = (double**)calloc(ny, sizeof(double*));
     double** dv_ss = (double**)calloc(ny, sizeof(double*));
 
-    double** H_old = (double**)calloc(ny, sizeof(double*));
+    double** Hx_old = (double**)calloc(ny, sizeof(double*));
+    double** Hy_old = (double**)calloc(ny, sizeof(double*));
 
     double** p = (double**)calloc(ny, sizeof(double*));             //cell centered
     double** phi = (double**)calloc(ny, sizeof(double*));
+
+    double** step1_mat_x = (double**)calloc(ny, sizeof(double*));
+    double** step1_mat_y = (double**)calloc(ny, sizeof(double*));
 
 
 
@@ -66,12 +90,15 @@ int main() {
         du_ss[i] = (double*)calloc(nx, sizeof(double));
         dv_ss[i] = (double*)calloc(nx, sizeof(double));
 
-        H_old[i] = (double*)calloc(nx, sizeof(double));
+        Hx_old[i] = (double*)calloc(nx, sizeof(double));
+        Hy_old[i] = (double*)calloc(nx, sizeof(double));
 
         p[i] = (double*)calloc(nx, sizeof(double));
         phi[i] = (double*)calloc(nx, sizeof(double));
+
+        step1_mat_x[i] = (double*)calloc(nx, sizeof(double));
+        step1_mat_y[i] = (double*)calloc(nx, sizeof(double));
         
-        printf("%d \n", i);
 
     }
 
@@ -86,11 +113,194 @@ int main() {
         }
         printf("\n");
     }
-    
 
 
 
-    /* ----------------------- Freeing Variables -------------------------------- */
+    /* -------------------------------------------------------------------------
+    ----------------------------------- Step 1 ---------------------------------
+    ------------------------------------------------------------------------- */
+
+    for (j = 0; j < Ny; j++) {
+        for (i = 0; i < Nx; i++) {
+            /* Calculating step1_mat_x */
+            if (i > 0 && j > 0 && i < (Nx - 1) && j < (Ny - 1)) {  /* At interior points except at the edges  */
+                u_cc = (u[j][i] + u[j][i + 1]) / 2;
+                u_cc_im1 = (u[j][i - 1] + u[j][i]) / 2;
+
+                u_s = (u[j][i] + u[j - 1][i]) / 2;
+                v_s = (v[j][i] + v[j][i - 1]) / 2;
+                u_s_jp1 = (u[j + 1][i] + u[j][i]) / 2;
+                v_s_jp1 = (v[j + 1][i] + v[j + 1][i - 1]) / 2;
+
+                Hx = ((pow(u_cc, 2) - pow(u_cc_im1, 2)) / dx) + (u_s_jp1 * v_s_jp1 - u_s * v_s) / dy;
+
+                step1_mat_x[j][i] = dt * (Hx * 3 / 2 - Hx_old[j][i] / 2 + (1 / Re) * ((u[j][i + 1] - 2 * u[j][i] + u[j][i - 1]) / pow(dx, 2) + (u[j + 1][i] - 2 * u[j][i] + u[j - 1][i]) / pow(dy, 2)));
+            }
+            else if (i == 0) {      /* At the left wall  */
+                u_cc = (u[j][i] + u[j][i + 1]) / 2;
+                u_cc_im1 = U_inlet;
+
+                u_s = U_inlet;
+                v_s = 0;
+                u_s_jp1 = (u[j + 1][i] + u[j][i]) / 2;
+                v_s_jp1 = 0;
+
+                Hx = ((pow(u_cc, 2) - pow(u_cc_im1, 2)) / dx) + (u_s_jp1 * v_s_jp1 - u_s * v_s) / dy;
+
+                step1_mat_x[j][i] = dt * (Hx * 3 / 2 - Hx_old[j][i] / 2 + (1 / Re) * ((u[j][i + 1] - 2 * u[j][i] + u[j][i]) / pow(dx, 2) + (u[j + 1][i] - 2 * u[j][i] + u[j - 1][i]) / pow(dy, 2)));
+            }
+            else if (i == Nx - 1) {     /* At the right wall  */
+                u_cc = u[j][i];
+                u_cc_im1 = (u[j][i - 1] + u[j][i]) / 2;
+
+                u_s = (u[j][i] + u[j - 1][i]) / 2;
+                v_s = (v[j][i] + v[j][i - 1]) / 2;
+                u_s_jp1 = (u[j + 1][i] + u[j][i]) / 2;
+                v_s_jp1 = (v[j + 1][i] + v[j + 1][i - 1]) / 2;
+
+                Hx = ((pow(u_cc, 2) - pow(u_cc_im1, 2)) / dx) + (u_s_jp1 * v_s_jp1 - u_s * v_s) / dy;
+
+                step1_mat_x[j][i] = dt * (Hx * 3 / 2 - Hx_old[j][i] / 2 + (1 / Re) * ((u[j][i] - 2 * u[j][i] + u[j][i - 1]) / pow(dx, 2) + (u[j + 1][i] - 2 * u[j][i] + u[j - 1][i]) / pow(dy, 2)));
+            }
+            else if (j == 0) {      /* At the bottom wall  */
+                u_cc = (u[j][i] + u[j][i + 1]) / 2;
+                u_cc_im1 = (u[j][i - 1] + u[j][i]) / 2;
+
+                u_s = u[j][i];     //this could also be set to zero
+                v_s = (v[j][i] + v[j][i - 1]) / 2;
+                u_s_jp1 = (u[j + 1][i] + u[j][i]) / 2;
+                v_s_jp1 = (v[j + 1][i] + v[j + 1][i - 1]) / 2;
+
+                Hx = ((pow(u_cc, 2) - pow(u_cc_im1, 2)) / dx) + (u_s_jp1 * v_s_jp1 - u_s * v_s) / dy;
+
+                step1_mat_x[j][i] = dt * (Hx * 3 / 2 - Hx_old[j][i] / 2 + (1 / Re) * ((u[j][i + 1] - 2 * u[j][i] + u[j][i - 1]) / pow(dx, 2) + (u[j + 1][i] - 2 * u[j][i] + u[j][i]) / pow(dy, 2)));
+            }
+            else if (j == Ny - 1) {    /* At the top lid  */
+                u_cc = (u[j][i] + u[j][i + 1]) / 2;
+                u_cc_im1 = (u[j][i - 1] + u[j][i]) / 2;
+
+                u_s = (u[j][i] + u[j - 1][i]) / 2;
+                v_s = (v[j][i] + v[j][i - 1]) / 2;
+                u_s_jp1 = 0;
+                v_s_jp1 = 0;
+
+                Hx = ((pow(u_cc, 2) - pow(u_cc_im1, 2)) / dx) + (u_s_jp1 * v_s_jp1 - u_s * v_s) / dy;
+
+                step1_mat_x[j][i] = dt * (Hx * 3 / 2 - Hx_old[j][i] / 2 + (1 / Re) * ((u[j][i + 1] - 2 * u[j][i] + u[j][i - 1]) / pow(dx, 2) + (u[j][i] - 2 * u[j][i] + u[j - 1][i]) / pow(dy, 2)));
+            }
+
+
+
+
+
+
+            /* Calculating step1_mat_y */
+            if (i > 0 && j > 0 && i < (Nx - 1) && j < (Ny - 1)) {  /* At interior points except at the edges  */
+                v_cc = (v[j][i] + v[j + 1][i]) / 2;
+                v_cc_jm1 = (v[j - 1][i] + v[j][i]) / 2;
+
+                u_s = (u[j][i] + u[j - 1][i]) / 2;
+                v_s = (v[j][i] + v[j][i - 1]) / 2;
+                u_s_ip1 = (u[j][i + 1] + u[j - 1][i + 1]) / 2;
+                v_s_ip1 = (v[j][i + 1] + v[j][i]) / 2;
+
+                Hy = ((pow(v_cc, 2) - pow(v_cc_jm1, 2)) / dy) + (u_s_ip1 * v_s_ip1 - u_s * v_s) / dx;
+
+                step1_mat_y[j][i] = dt * (Hy * 3 / 2 - Hy_old[j][i] / 2 + (1 / Re) * ((v[j][i + 1] - 2 * v[j][i] + v[j][i - 1]) / pow(dx, 2) + (v[j + 1][i] - 2 * v[j][i] + v[j - 1][i]) / pow(dy, 2)));
+            }
+            else if (j == 0) {    /* At the bottom wall  */
+                v_cc = (v[j][i] + v[j + 1][i]) / 2;
+                v_cc_jm1 = 0;
+
+                u_s = 0;
+                v_s = (v[j][i] + v[j][i - 1]) / 2;
+                u_s_ip1 = 0;
+                v_s_ip1 = 0;
+
+                Hy = ((pow(v_cc, 2) - pow(v_cc_jm1, 2)) / dy) + (u_s_ip1 * v_s_ip1 - u_s * v_s) / dx;
+
+                step1_mat_y[j][i] = dt * (Hy * 3 / 2 - Hy_old[j][i] / 2 + (1 / Re) * ((v[j][i + 1] - 2 * v[j][i] + v[j][i - 1]) / pow(dx, 2) + (v[j + 1][i] - 2 * v[j][i] + v[j][i]) / pow(dy, 2)));
+            }
+            else if (j == Ny - 1) {     /* At the top wall  */
+                v_cc = (v[j][i] + 0) / 2;
+                v_cc_jm1 = (v[j - 1][i] + v[j][i]) / 2;
+
+                u_s = (u[j][i] + u[j - 1][i]) / 2;
+                v_s = (v[j][i] + v[j][i - 1]) / 2;
+                u_s_ip1 = (u[j][i + 1] + u[j - 1][i + 1]) / 2;
+                v_s_ip1 = (v[j][i + 1] + v[j][i]) / 2;
+
+                Hy = ((pow(v_cc, 2) - pow(v_cc_jm1, 2)) / dy) + (u_s_ip1 * v_s_ip1 - u_s * v_s) / dx;
+
+                step1_mat_y[j][i] = dt * (Hy * 3 / 2 - Hy_old[j][i] / 2 + (1 / Re) * ((v[j][i + 1] - 2 * v[j][i] + v[j][i - 1]) / pow(dx, 2) + (v[j][i] - 2 * v[j][i] + v[j - 1][i]) / pow(dy, 2)));
+            }
+            else if (i == 0) {      /* At left wall  */
+                v_cc = (v[j][i] + v[j + 1][i]) / 2;
+                v_cc_jm1 = (v[j - 1][i] + v[j][i]) / 2;
+
+                u_s = U_inlet;
+                v_s = 0;
+                u_s_ip1 = (u[j][i + 1] + u[j - 1][i + 1]) / 2;
+                v_s_ip1 = (v[j][i + 1] + v[j][i]) / 2;
+
+                Hy = ((pow(v_cc, 2) - pow(v_cc_jm1, 2)) / dy) + (u_s_ip1 * v_s_ip1 - u_s * v_s) / dx;
+
+                step1_mat_y[j][i] = dt * (Hy * 3 / 2 - Hy_old[j][i] / 2 + (1 / Re) * ((v[j][i + 1] - 2 * v[j][i] + v[j][i]) / pow(dx, 2) + (v[j + 1][i] - 2 * v[j][i] + v[j - 1][i]) / pow(dy, 2)));
+            }
+            else if (i == Nx - 1) {     /* At the right wall  */
+                v_cc = (v[j][i] + v[j + 1][i]) / 2;
+                v_cc_jm1 = (v[j - 1][i] + v[j][i]) / 2;
+
+                u_s = (u[j][i] + u[j - 1][i]) / 2;
+                v_s = (v[j][i] + v[j][i - 1]) / 2;
+                u_s_ip1 = u_s;
+                v_s_ip1 = v_s;
+
+                Hy = ((pow(v_cc, 2) - pow(v_cc_jm1, 2)) / dy) + (u_s_ip1 * v_s_ip1 - u_s * v_s) / dx;
+
+                step1_mat_y[j][i] = dt * (Hy * 3 / 2 - Hy_old[j][i] / 2 + (1 / Re) * ((v[j][i] - 2 * v[j][i] + v[j][i - 1]) / pow(dx, 2) + (v[j + 1][i] - 2 * v[j][i] + v[j - 1][i]) / pow(dy, 2)));
+            }
+        }
+    }
+
+
+
+
+
+    /* -------------------------------------------------------------------------
+    ----------------------------------- Step 2 ---------------------------------
+    ------------------------------------------------------------------------- */
+
+
+
+
+
+
+
+
+
+    /* -------------------------------------------------------------------------
+    ----------------------------------- Step 3 ---------------------------------
+    ------------------------------------------------------------------------- */
+
+
+
+
+
+
+
+
+
+
+    /* -------------------------------------------------------------------------
+    ------------------------------ Scalar Transport ----------------------------
+    ------------------------------------------------------------------------- */
+
+
+
+    /* -------------------------------------------------------------------------
+    --------------------------- Freeing Variables ------------------------------
+    ------------------------------------------------------------------------- */
     for (i = 0; i < Ny; i++) {
         free(u[i]);
         free(v[i]);
@@ -102,10 +312,14 @@ int main() {
         free(du_ss[i]);
         free(dv_ss[i]);
 
-        free(H_old[i]);
+        free(Hx_old[i]);
+        free(Hy_old[i]);
 
         free(p[i]);
         free(phi[i]);
+
+        free(step1_mat_x[i]);
+        free(step1_mat_y[i]);
     }
 
     free(u);
@@ -118,9 +332,13 @@ int main() {
     free(du_ss);
     free(dv_s);
 
-    free(H_old);
+    free(Hx_old);
+    free(Hy_old);
 
     free(p);
     free(phi);
+
+    free(step1_mat_x);
+    free(step1_mat_y);
 
 }
